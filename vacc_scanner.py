907449e5a632ad40e581f9e4f010994a6590d47b
Ritesh Ghosh <instruritesh@gmail.com>
2021-05-09 22:32:47 +0530

    Scan for multiple dates added with random time sleep

--- vacc_scanner.py
+++ vacc_scanner.py
@@ -1,6 +1,7 @@
 import time
 import requests
 import smtplib
+import random
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -9,12 +10,13 @@ from email.mime.text import MIMEText
 """
 Find district Id from https://getjab.in/ district (sorted by alphabet - showing district id in front)
 """
-DISTRICT_ID = "69"  # Karimganj
-DATE = "09-05-2021"     # Double digit format DD-MM-YYYY
+DISTRICT_ID = "725"  # Kolkata
+DATE_LIST = ["10-05-2021", "11-05-2021"]     # Double digit format DD-MM-YYYY
 MIN_AGE_LIMIT = 18
-COWIN_API = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=" + DISTRICT_ID + "&date=" + DATE
+COWIN_API = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=" + DISTRICT_ID + "&date="
 
-SCAN_TIME_INTERVAL = 60  # 60 seconds
+SCAN_TIME_INTERVAL_MIN = 60  # 60 seconds
+SCAN_TIME_INTERVAL_MAX = 90  # 90 seconds
 
 # Notification config
 """
@@ -27,15 +29,16 @@ SCAN_TIME_INTERVAL = 60  # 60 seconds
 5. MAIL_TO_GMAIL_ID is the list of mail ids to which mail will be sent (can be single and same as sign in)
 """
 MAIL_NOTIFICATION_ALLOWED = True
-GMAIL_USER = 'xxx.yyy@gmail.com'
+GMAIL_USER = 'abc.pqr@gmail.com'
 GMAIL_PASSWORD = 'abcd1234'
-MAIL_TO_GMAIL_IDS = ['xxx.yyy@gmail.com', 'ppp.rrr@gmail.com']
+MAIL_TO_GMAIL_IDS = ['abc.pqr@gmail.com', 'xxx.yyy@gmail.com']
+APPEND_UTC_TIME_IN_SUBJECT = True   # This will generate new subject in mail if True
 
 
 class CowinScanner():
 
     @classmethod
-    def make_request(cls):
+    def make_request(cls, date):
         data = None
 
         # EXTREMELY IMPORTANT - FAKE THE REQUEST AS IF FROM BROWSER CHROME
@@ -56,7 +59,9 @@ class CowinScanner():
                       'Accept-Language': 'en-US,en-IN;q=0.9,en;q=0.8'}
 
         try:
-            response = requests.get(COWIN_API, headers=my_headers, verify=False)
+            url = COWIN_API + date
+            print("===== hit API: ", url)
+            response = requests.get(url, headers=my_headers, verify=False)
             status_code = response.status_code
             if status_code == 200:
                 data = response.json()
@@ -86,9 +91,9 @@ class CowinScanner():
                     for session in sessions_list:
                         availability = session.get('available_capacity')
                         age_limit = session.get('min_age_limit')
-                        if availability > 0 and age_limit >= MIN_AGE_LIMIT:
+                        if availability >= 0 and age_limit >= MIN_AGE_LIMIT:
                             slot_available = slot_available + availability
-                            notify_dict[center_name] = slot_available
+                            notify_dict[center_name] = {'slots': slot_available, 'age': age_limit}
         return notify_dict
 
     @classmethod
@@ -97,19 +102,21 @@ class CowinScanner():
         for key in notify_dict.keys():
             center_name = key
             # center_name = center_name[0:6]  # strip chars for SMS
-            available = notify_dict[key]
-            notif_str = notif_str + center_name + ":" + str(available) + "," + "\n"
+            available = notify_dict[key].get('slots')
+            age_limit = notify_dict[key].get('age')
+            notif_str = notif_str + center_name + ": slots-" + str(available) + ", min age-" + str(age_limit) + ". " + "\n"
         return notif_str
 
     @classmethod
-    def send_notification_mail(cls, notify_dict):
+    def send_notification_mail(cls, notify_dict, date):
         body = cls.format_notif_msg(notify_dict)
 
         # Setup the MIME
         message = MIMEMultipart()
         message['From'] = GMAIL_USER
         message['To'] = MAIL_TO_GMAIL_IDS[0]
-        message['Subject'] = 'CoWin Vaccine slot availability'  # The subject line
+        time_now = "[" + str(round(time.time())) + "]" if APPEND_UTC_TIME_IN_SUBJECT else ""
+        message['Subject'] = "[CoWin Vaccine][" + date + "]: slot availability " + time_now  # The subject line
 
         mail_content = body
         message.attach(MIMEText(mail_content, 'plain'))
@@ -142,21 +149,27 @@ class CowinScanner():
             print(err)
 
     @classmethod
+    def process_date(cls, date):
+        # Request API
+        data = cls.make_request(date)
+
+        # Parse data
+        notify_dict = cls.parse_data(data)
+
+        # Send notification
+        if len(notify_dict.keys()) > 0 and MAIL_NOTIFICATION_ALLOWED:
+            cls.send_notification_mail(notify_dict, date)
+
+    @classmethod
     def scan(cls):
         # Infinite loop - careful - cdn may block if timer not proper
         while True:
-            # Request API
-            data = cls.make_request()
+            for date_search in DATE_LIST:
+                cls.process_date(date_search)
 
-            # Parse data
-            notify_dict = cls.parse_data(data)
-
-            # Send notification
-            if len(notify_dict.keys()) > 0 and MAIL_NOTIFICATION_ALLOWED:
-                cls.send_notification_mail(notify_dict)
-
-            # sleep for time secs
-            time.sleep(SCAN_TIME_INTERVAL)
+                # IMPORTANT: sleep for random time secs, ditch pattern crawler
+                delay = random.randint(SCAN_TIME_INTERVAL_MIN, SCAN_TIME_INTERVAL_MAX)
+                time.sleep(delay)
 
 
 """
